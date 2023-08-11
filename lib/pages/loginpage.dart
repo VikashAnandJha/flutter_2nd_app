54dfbb62461757190b71d2015d33712f6859966b
VikashAnandJha <mailtovikashjha@gmail.com>
2023-08-11 05:38:10 +0530

    form validation

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  var name = "";
+
+  final _formKey = GlobalKey<FormState>();
+
+  moveToNext(BuildContext context) {
+    if (_formKey.currentState != null && _formKey.currentState!.validate()) {
+      Navigator.pushNamed(context, "/home");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,38 +26,57 @@ class LoginPage extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(18.0),
         child: Center(
-          child: Column(children: [
-            Image.asset(
-              "assets/images/login.png",
-              fit: BoxFit.cover,
-            ),
-            const Text(
-              "Login Page",
-              style: TextStyle(fontSize: 20),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                hintText: "Enter Your Name",
-                labelText: "Username",
+          child: Form(
+            key: _formKey,
+            child: Column(children: [
+              Image.asset(
+                "assets/images/login.png",
+                fit: BoxFit.cover,
               ),
-            ),
-            TextFormField(
-              obscureText: true,
-              decoration: const InputDecoration(
-                hintText: "Enter Your Password",
-                labelText: "Password",
+              Text(
+                "Login Page $name",
+                style: TextStyle(fontSize: 20),
               ),
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            ElevatedButton(
-                onPressed: () => {Navigator.pushNamed(context, "/home")},
-                style: TextButton.styleFrom(
-                  minimumSize: const Size(250, 40),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Username is required";
+                  }
+                  return null; // Return null when the value is valid
+                },
+                onChanged: (value) {
+                  name = value;
+                  setState(() {});
+                },
+                decoration: const InputDecoration(
+                  hintText: "Enter Your Name",
+                  labelText: "Username",
                 ),
-                child: const Text("Login "))
-          ]),
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Password is required";
+                  }
+                  return null; // Return null when the value is valid
+                },
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: "Enter Your Password",
+                  labelText: "Password",
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              ElevatedButton(
+                  onPressed: () => moveToNext(context),
+                  style: TextButton.styleFrom(
+                    minimumSize: const Size(250, 40),
+                  ),
+                  child: const Text("Login "))
+            ]),
+          ),
         ),
       ),
     );
