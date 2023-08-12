ccd3854253065ff8e7af1f8b6d2efc6d2dab4f75
VikashAnandJha <mailtovikashjha@gmail.com>
2023-08-12 07:50:30 +0530

    grid height width

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -40,28 +40,57 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("My E commerce"),
+        title: Text("GrayMart"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
             ? GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+
+                  crossAxisSpacing: 5, // Adjust the spacing between columns
+                  mainAxisSpacing: 10, // Adjust the spacing between rows
+                  childAspectRatio:
+                      0.8, // Adjust the width-to-height ratio of each grid item
+                ),
                 itemBuilder: (context, index) {
                   final item = CatalogModel.items[index];
-
+                  var title = item.title.toString().length > 20
+                      ? item.title.toString().substring(0, 20)
+                      : item.title;
                   return Card(
                     child: GridTile(
-                      child: Container(
-                          padding: EdgeInsets.all(20),
-                          margin: EdgeInsets.all(10),
-                          child: Image.network(item.image)),
-                      footer: Container(
+                      footer: Padding(
+                        padding: const EdgeInsets.all(8.0),
                         child: Column(
                           children: [
-                            Text(item.title.toString()),
+                            Text(
+                              title,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                              ),
+                            ),
                             Text("\$ ${item.price}"),
+                          ],
+                        ),
+                      ),
+                      child: Container(
+                        padding: EdgeInsets.all(0),
+                        margin: EdgeInsets.all(0),
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              child: Image.network(
+                                item.image,
+
+                                height: 180,
+
+                                fit: BoxFit
+                                    .cover, // Adjust the image fit as needed
+                              ),
+                            ),
                           ],
                         ),
                       ),
@@ -70,7 +99,7 @@ class _HomePageState extends State<HomePage> {
                 },
                 itemCount: CatalogModel.items.length,
               )
-            : Center(
+            : const Center(
                 child: CircularProgressIndicator(),
               ),
       ),
