f22e47ff0e98f5b1afd0a4176c9dfdd756747061
VikashAnandJha <mailtovikashjha@gmail.com>
2023-08-12 07:05:40 +0530

    load data from local JSON

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,22 +1,76 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 class CatalogModel {
-  static final items = [
-    Item(
-        id: 1,
-        title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-        image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-        price: 109.95)
-  ];
+  static var items = [];
 }
 
 class Item {
   final int id;
   final String title;
   final String image;
-  final double price;
+  final num price;
+  Item({
+    required this.id,
+    required this.title,
+    required this.image,
+    required this.price,
+  });
 
-  Item(
-      {required this.id,
-      required this.title,
-      required this.image,
-      required this.price});
+  Item copyWith({
+    int? id,
+    String? title,
+    String? image,
+    num? price,
+  }) {
+    return Item(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      image: image ?? this.image,
+      price: price ?? this.price,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'title': title,
+      'image': image,
+      'price': price,
+    };
+  }
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map['id'] as int,
+      title: map['title'] as String,
+      image: map['image'] as String,
+      price: map['price'] as num,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Item.fromJson(String source) =>
+      Item.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'Item(id: $id, title: $title, image: $image, price: $price)';
+  }
+
+  @override
+  bool operator ==(covariant Item other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id &&
+        other.title == title &&
+        other.image == image &&
+        other.price == price;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^ title.hashCode ^ image.hashCode ^ price.hashCode;
+  }
 }
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,11 +1,40 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_2nd_app/models/catalog.dart';
 import 'package:flutter_2nd_app/pages/widgets/drawer.dart';
 import 'package:flutter_2nd_app/pages/widgets/item.widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   int days = 30;
+
   String myName = "vikash jha";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    loadData();
+  }
+
+  loadData() async {
+    var data = await rootBundle.loadString("assets/files/catalog.json");
+    var products = jsonDecode(data);
+
+    CatalogModel.items =
+        List.from(products).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
+
+    // print(json[0]);
+  }
+
   // final dummyList=List.generate(5, (index) => CatalogModel.items[0]);
   @override
   Widget build(BuildContext context) {
@@ -15,14 +44,18 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: ListView.builder(
-          itemCount: CatalogModel.items.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(
-              item: CatalogModel.items[index],
-            );
-          },
-        ),
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ? ListView.builder(
+                itemCount: CatalogModel.items.length,
+                itemBuilder: (context, index) {
+                  return ItemWidget(
+                    item: CatalogModel.items[index],
+                  );
+                },
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
